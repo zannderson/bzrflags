ef04754466c64ec2a2b7e5545e9b148c51416c4a
Jordan Blaise Handy <jbh234@club.cs.byu.edu>
2014-09-23 22:46:13 -0600

    unfinished tank population work

--- bzrflags/Main.cs
+++ bzrflags/Main.cs
@@ -6,23 +6,25 @@ namespace bzrflags
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{			
-			//PFAgent pf = new PFAgent(50103,1);
-			//pf.updateTanks();
+		{
+			/*
+			TelnetConnection connection = new TelnetConnection(50103);
+			connection.ReceiveMessage();
+			string response = connection.SendMessage("agent 1");
+			string command;
+			while(true)
+			{
+				Console.Out.Write("Input a command: ");
+				command = Console.In.ReadLine();
+				connection.SendMessage(command);
+				Console.Out.WriteLine();
+				Console.Out.WriteLine(connection.ReceiveMessage());
+			}
+			*/
 			
-			TelnetConnection connection = new TelnetConnection(51997);
-			connection.SendMessage("agent 1", false);
-			string obstacles = connection.SendMessage("obstacles", true);
-			List<PotentialField> obstacleFields = ObstacleCollection.GetFieldsForObstacles(obstacles);
-			string flags = connection.SendMessage("flags", true);
-			List<PotentialField> flagFields = Flags.GetFlagFields(flags);
-			PotentialFieldsCollection collection = new PotentialFieldsCollection();
-			List<PotentialField> manualFields = new List<PotentialField>();
-			manualFields.Add(new AttractField(0.0, 370.0, 1.0, 0.1, 300));
-			//collection.PotentialFields = manualFields;
-			collection.PotentialFields = flagFields;
-			//collection.PotentialFields.AddRange(flagFields);
-			GnuPlotGenerator.PlotMyFields("fields", collection);
+			PFAgent pf = new PFAgent(50103,1);
+			pf.populateTanks();
+			
 			
 			/*
 			List<PotentialField> fields = new List<PotentialField>();
--- bzrflags/Tank.cs
+++ bzrflags/Tank.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace bzrflags
 {
@@ -13,32 +14,19 @@ namespace bzrflags
 	public class Tank
 	{
 			
-		private int _index;
-		private int _callsign;
-		private int _shotsAvailable;
-		private int _timeToReload;
-		private bool _hasFlag;
-		private int _xPosition;
-		private int _yPosition;
-		private double _angle;
-		private int _angelVelocity;
-		private TelnetConnection _connection;
+		private int _index {set;get;}
+		private string _callsign {set;get;}
+		private int _shotsAvailable {set;get;}
+		private int _timeToReload {set;get;}
+		private bool _hasFlag {set;get;}
+		private int _xPosition {set;get;}
+		private int _yPosition {set;get;}
+		private double _angle {set;get;}
+		private double _angelVelocity {set;get;}
+		private TelnetConnection _connection {set;get;}
 		
-		public Tank (TelnetConnection connection, int index, int xPos, int yPos, double angle, int angleVelocity)
-		{
-			_connection = connection;
-			_index = index;
-			_xPosition = xPos;
-			_yPosition = yPos;
-			_angle = angle;
-			_angelVelocity = angleVelocity;
-			
-			//callsign?
-			
-			_shotsAvailable = 3;
-			_timeToReload = 0;
-			_hasFlag = false;
-			
+		public Tank ()
+		{		
 		}
 		
 		public void updateMyPosition(int xPos, int yPos, double angle, int angleVelocity)
@@ -54,7 +42,7 @@ namespace bzrflags
 		{
 			double targetAngle = Math.Atan2(targetY - _yPosition, targetX - _xPosition);
 			double relativeAngle = normalizeAngle(targetAngle - _angle);
-		 	_connection.SendMessage(_index  + " 1 " + (2 * relativeAngle), true );
+		 	_connection.SendMessage(_index  + " 1 " + (2 * relativeAngle) );
             Console.Out.WriteLine(_connection.ReceiveMessage());	
 		}
 		
@@ -73,6 +61,70 @@ namespace bzrflags
 			
 			return angle;
 		}
+		
+	public static List<Tank> createTanks(string rawTanksData)
+	{
+		List<Tank> myTanks = new List<Tank>();
+			
+		string[] splitTankData = rawTanksData.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+			
+			foreach(string individualTankData in splitTankData)
+			{
+				if(individualTankData == "begin" || individualTankData == "end")
+					continue;
+				
+				string[] tokens = individualTankData.Split(null);
+				
+				int tankIndex = int.Parse (tokens[1]);
+				string callsign = tokens[2];
+				string status = tokens[3];
+				int shotsAvailable = int.Parse(tokens[4]);
+				int timeToReload = int.Parse(tokens[5]);
+				char flag = char.Parse(tokens[6]);
+				int xPos = int.Parse(tokens[34]);
+				int yPos = int.Parse (tokens[35]);
+				double angle = double.Parse (tokens[36]);
+				double velocityX = double.Parse (tokens[37]);
+				double velocityY = double.Parse (tokens[38]);
+				double angleVelocity = double.Parse (tokens[39]);
+				
+				
+				//convert flag status to bool
+					
+				//convert status to enum
+				
+				
+				//create tank, add it to myTanks
+				Tank tempTank = new Tank()
+				{
+					_index = tankIndex,
+					_callsign = callsign,
+					//connection????
+					//status?????
+					//flag?????
+					
+					_shotsAvailable = shotsAvailable,
+					_timeToReload = timeToReload,
+					_xPosition = xPos,
+					_yPosition = yPos,
+					_angle = angle,
+					//velocityX
+					//""Y
+					_angelVelocity = angleVelocity
+					
+				};
+				
+				
+			}
+		return myTanks;
 	}
+		
+		
+		
+	}
+	
+	
+	
+	
 }
 
